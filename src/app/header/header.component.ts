5e26f3e5dbc1ab59524e72186612071bd7dfcd0d
adriiberjaga <adriabdd@gmail.com>
2024-10-24 21:23:53 +0200

    adri-navbar-no acabado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  activeSection: string = 'inicio'; // Sección activa inicial
 
+  // Método para verificar si la sección está activa
+  isActive(section: string): boolean {
+    return this.activeSection === section;
+  }
+
+  // Escuchar el evento de desplazamiento
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = document.querySelectorAll('section');
+    const scrollPos = window.scrollY + 50; // Ajuste para el desplazamiento
+
+    sections.forEach((section: HTMLElement) => {
+      if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) {
+        this.activeSection = section.id; // Actualiza la sección activa
+      }
+    });
+  }
 }
