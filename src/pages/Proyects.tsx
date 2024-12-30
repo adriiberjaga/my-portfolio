4e6c2a7f7ceab666cfb675df4745663baf895db3
adriiberjaga <adriabdd@gmail.com>
2024-12-30 13:36:30 +0100

    skills section finish, and add content to proyect section

--- src/pages/Proyects.tsx
+++ src/pages/Proyects.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 import styles from "./Proyects.module.css";
+
 import "aos/dist/aos.css";
 import AOS from "aos";
-import fotoAngular from '../assets/angular-icon.png';
-import fotoCss from '../assets/css-icon.png';
-import fotoHtml from '../assets/html-icon.png';
-import fotoReact from '../assets/react-icon.png';
-import fotoTs from '../assets/ts-icon.png';
-import todolist from '../assets/todolist.png';
-import ecomerce from '../assets/ecomerce.png';
+
+import fotoAngular from "../assets/angular-icon.png";
+import fotoCss from "../assets/css-icon.png";
+import fotoHtml from "../assets/html-icon.png";
+import fotoReact from "../assets/react-icon.png";
+import fotoTs from "../assets/ts-icon.png";
+import todolist from "../assets/todolist.png";
+import ecomerce from "../assets/ecomerce.png";
+import close from "../assets/close-icon.png";
 
 // Mover la interfaz fuera del componente
 interface Project {
@@ -30,13 +34,16 @@ const projects: Project[] = [
       { name: "Node.js", icon: "https://via.placeholder.com/30" },
     ],
     desc: "Descripción del Proyecto 1",
-    ref: "https://to-do-list-react-kappa-roan.vercel.app/"
+    ref: "https://to-do-list-react-kappa-roan.vercel.app/",
   },
   {
     image: ecomerce,
     title: "Proyecto 2",
     languages: [
-      { name: "Python", icon: "https://cdn.jsdelivr.net/npm/@iconscout/unicons/svg/python.svg" },
+      {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/npm/@iconscout/unicons/svg/python.svg",
+      },
       { name: "Django", icon: "https://via.placeholder.com/30" },
       { name: "PostgreSQL", icon: "https://via.placeholder.com/30" },
     ],
@@ -46,6 +53,22 @@ const projects: Project[] = [
 ];
 
 function Proyects() {
+  const [openCard, setOpenCard] = useState<string | null>(null);
+  const opCard = (project: string) => {
+    setOpenCard(project);
+  };
+  const closeCard = () => {
+    setOpenCard(null);
+  };
+  const [isClosing, setIsClosing] = useState(false);
+  const handleClose = () => {
+    setIsClosing(true); // Activa la animación de cierre
+    setTimeout(() => {
+      closeCard(); // Cierra el modal después de la animación
+      setIsClosing(false); // Restablece el estado
+    }, 500); // Asegúrate de que el tiempo coincide con la duración de la animación
+  };
+
   useEffect(() => {
     AOS.init({
       duration: 1200,
@@ -54,32 +77,105 @@ function Proyects() {
   }, []);
 
   return (
-    <div className={styles.content}>
-      <h3 className={styles.title}>Proyectos</h3>
-      <div className={styles.projects}>
-        {projects.map((project, index) => (
-          <div data-aos="flip-left" key={index} className={styles["project-card"]}>
-            <img className={styles["project-card__img"]} src={project.image} alt={project.title} />
-            <h3 className={styles["project-card__title"]}>{project.title}</h3>
-            <div className={styles["languages"]}>
-              {project.languages.map((language, idx) => (
-                <img
-                  key={idx}
-                  src={language.icon}
-                  alt={language.name}
-                  title={language.name}
-                  className={styles["language-icon"]}
-                />
-              ))}
+    <>
+      <div className={styles.content}>
+        <h3 className={styles.title}>Proyectos</h3>
+        <div className={styles.projects}>
+          {projects.map((project, index) => (
+            <div
+              data-aos="flip-left"
+              key={index}
+              className={styles["project-card"]}
+            >
+              <img
+                className={styles["project-card__img"]}
+                src={project.image}
+                alt={project.title}
+              />
+              <h3 className={styles["project-card__title"]}>{project.title}</h3>
+              <div className={styles["languages"]}>
+                {project.languages.map((language, idx) => (
+                  <img
+                    key={idx}
+                    src={language.icon}
+                    alt={language.name}
+                    title={language.name}
+                    className={styles["language-icon"]}
+                  />
+                ))}
+              </div>
+              <p className={styles["project-card__desc"]}>{project.desc}</p>
+              <button
+                onClick={() => setOpenCard(project.title)}
+                className={styles["project-card__ver"]}
+                title="Ver más"
+              >
+                Ver más..
+              </button>
+              <a
+                className={styles["project-card__btn"]}
+                href={project.ref}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visitar sitio web
+              </a>
             </div>
-            <p className={styles["project-card__desc"]}>{project.desc}</p>
-            <a className={styles["project-card__btn"]} href={project.ref} target="_blank" rel="noopener noreferrer">
-              Ver Proyecto
-            </a>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      {openCard && (
+        <div
+          data-aos={isClosing ? "zoom-in" : "zoom-in"}
+          data-aos-duration="500"
+          className={`${styles["modal"]} ${isClosing ? styles["closing"] : ""}`}
+          onClick={closeCard}
+        >
+          <div
+            className={styles["modal-content"]}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <h2 className={styles["modal-title"]}>{openCard}</h2>
+            <div className={styles["modal-img"]}>
+              {projects.find((project) => project.title === openCard)
+                ?.image && (
+                <img
+                  src={
+                    projects.find((project) => project.title === openCard)
+                      ?.image
+                  }
+                  alt={openCard}
+                  className={styles["modal-img__content"]}
+                />
+              )}
+            </div>
+            <div className={styles["languages"]}>
+              {projects
+                .find((project) => project.title === openCard)
+                ?.languages.map((language, idx) => (
+                  <img
+                    key={idx}
+                    src={language.icon}
+                    alt={language.name}
+                    title={language.name}
+                    className={styles["language-icon"]}
+                  />
+                ))}
+            </div>
+            <p className={styles["description"]}>
+              {projects.find((project) => project.title === openCard)?.desc}
+            </p>
+            <button
+              title="close"
+              onClick={handleClose}
+              className={styles["modal-close"]}
+            >
+              <img className={styles.closebtn} src={close} alt="" />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,73 +1,141 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 import "aos/dist/aos.css";
 import AOS from "aos";
 
-import styles from './Skills.module.css';
-import fotoAngular from '../assets/angular-icon.png';
-import fotoCss from '../assets/css-icon.png';
-import fotoHtml from '../assets/html-icon.png';
-import fotoReact from '../assets/react-icon.png';
-import fotoTs from '../assets/ts-icon.png';
-import fotoJs from '../assets/js-icon.png';
-import { useEffect } from "react";
+import styles from "./Skills.module.css";
+import fotoAngular from "../assets/angular-icon.png";
+import fotoCss from "../assets/css-icon.png";
+import fotoHtml from "../assets/html-icon.png";
+import fotoReact from "../assets/react-icon.png";
+import fotoTs from "../assets/ts-icon.png";
+import fotoJs from "../assets/js-icon.png";
+import close from "../assets/close-icon.png";
 
 function Skills() {
-
-      useEffect(() => {
-        AOS.init({
-          duration: 2300,
-          once: false,
-        });
-      }, []);
-      const getAnimation = (index: number) => {
-        if (window.innerWidth <= 768) { 
-            return "fade-down";
-        }  else {
-            switch (index) {
-                case 0:
-                    return "fade-right";
-                case 1:
-                    return "fade-down";
-                case 2:
-                    return "fade-left";
-                case 3:
-                    return "fade-up";
-                default:
-                    return "fade-right";
-            }
-        }
-         
+  //pop up de skills
+  const [selectedSkill, setSelectedSkill] = useState<string | null>(null);
+  const [selectedSkillImg, setSelectedSkillImg] = useState<string>("");
+  const openSkill = (skill: string, img: string) => {
+    setSelectedSkill(skill);
+    setSelectedSkillImg(img);
+  };
+  
+  const closeSkill = () => {
+    setSelectedSkill(null);
+  };
+  const infoSkills = (skill: string) => {
+    switch (skill) {
+        case "HTML":
+            return"Experiencia de 2 años";
+        case "JavaScript":
+            return"Experiencia de 1 año";
+        case "HTML":
+            return"Experiencia de 1 año";
+        case "CSS":
+            return"Experiencia de 1 año";
+        case "TypeScript":
+            return"Experiencia de 1 año";
+        case "Angular":
+            return"Experiencia de 1 año";
     }
-    return (
-        <div className={styles.main}>
-    <div className={styles.firstCont}>
+  }
+  //animaciones
+  useEffect(() => {
+    AOS.init({
+      duration: 2300,
+      once: false,
+    });
+  }, []);
+  const getAnimation = (index: number) => {
+    if (window.innerWidth <= 768) {
+      return "fade-down";
+    } else {
+      switch (index) {
+        case 0:
+          return "fade-right";
+        case 1:
+          return "fade-down";
+        case 2:
+          return "fade-left";
+        case 3:
+          return "fade-up";
+        default:
+          return "fade-right";
+      }
+    }
+  };
+
+
+  return (
+    <>
+
+    <div className={styles.main}>
+      <div className={styles.firstCont}>
         <div data-aos={getAnimation(0)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoHtml} alt="" />
-            <p className={styles.iconText}>HTML</p>
+          <img
+            onClick={() => openSkill("HTML", fotoHtml)}
+            className={styles.icons}
+            src={fotoHtml}
+            alt=""
+          />
+          <p className={styles.iconText}>HTML</p>
         </div>
         <div data-aos={getAnimation(1)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoJs} alt="" />
-            <p className={styles.iconText}>JavaScript</p>
+          <img onClick={() => openSkill("JavaScript", fotoJs)} className={styles.icons} src={fotoJs} alt="" />
+          <p className={styles.iconText}>JavaScript</p>
         </div>
         <div data-aos={getAnimation(2)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoCss} alt="" />
-            <p className={styles.iconText}>CSS</p>
+          <img
+            onClick={() => openSkill("CSS", fotoCss)}
+            className={styles.icons}
+            src={fotoCss}
+            alt=""
+          />
+          <p className={styles.iconText}>CSS</p>
         </div>
-    </div>        
-    <div className={styles.secondCont}>
+      </div>
+      <div className={styles.secondCont}>
         <div data-aos={getAnimation(0)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoReact} alt="" />
-            <p className={styles.iconText}>React</p>
+          <img
+            onClick={() => openSkill("React", fotoReact)}
+            className={styles.icons}
+            src={fotoReact}
+            alt=""
+          />
+          <p className={styles.iconText}>React</p>
         </div>
         <div data-aos={getAnimation(3)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoTs} alt="" />
-            <p className={styles.iconText}>TypeScript</p>
+          <img onClick={() => openSkill("TypeScript", fotoTs)} className={styles.icons} src={fotoTs} alt="" />
+          <p className={styles.iconText}>TypeScript</p>
         </div>
         <div data-aos={getAnimation(2)} className={styles.iconContainer}>
-            <img  className={styles.icons} src={fotoAngular} alt="" />
-            <p className={styles.iconText}>Angular</p>
+          <img
+            onClick={() => openSkill("Angular", fotoAngular)}
+            className={styles.icons}
+            src={fotoAngular}
+            alt=""
+          />
+          <p className={styles.iconText}>Angular</p>
         </div>
-    </div>        
-</div>
-    );
+      </div>
+    </div>
+    {selectedSkill && (
+       <div className={styles.modalOverlay} onClick={closeSkill}>
+       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}
+        data-aos="zoom-in" data-aos-duration="300">
+       <img className={styles.modalContentImg} src={selectedSkillImg} alt="" />
+         <h2>{selectedSkill}</h2>
+         <p>
+              {infoSkills(selectedSkill)}
+         </p>
+         <button title="close" onClick={closeSkill}><img className={styles.closebtn} src={close} alt="" /></button>
+       </div>
+     </div>  
+    )}
+    </>
+
+  );
 }
 export default Skills;
